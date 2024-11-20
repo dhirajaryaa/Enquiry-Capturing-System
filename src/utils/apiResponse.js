25145773bd771ed596190247603686eae6fe6739
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-11-20 19:16:58 +0530

    feat: creating api Response utils

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,9 @@
+class apiResponse {
+    constructor(statusCode, data, message = 'Success') {
+      (this.statusCode = statusCode),
+        (this.message = message),
+        (this.data = data),
+        (this.success = statusCode < 400);
+    }
+  }
+  
